d9f8df680c312269a5a86f72f9049b497bb758ec
Atul Kumar <atulkumar@Atuls-MacBook-Air.local>
2020-06-11 19:49:24 +0530

    Netflix Assignment

--- app/src/main/java/com/example/assignment/MainActivity.java
+++ app/src/main/java/com/example/assignment/MainActivity.java
@@ -10,13 +10,15 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     //Urls of Movies posters
-    final String url1="https://shorturl.at/vwAQV";
-    final String url2="https://shorturl.at/FGHS6";
-    final String url3="https://shorturl.at/egsJY";
-    final String url4="https://shorturl.at/abeO1";
-    final String url5="https://shorturl.at/dmAHW";
+    final String url1="https://m.media-amazon.com/images/M/MV5BOTk5ODg0OTU5M15BMl5BanBnXkFtZTgwMDQ3MDY3NjM@._V1_SX300.jpg";
+    final String url2="https://m.media-amazon.com/images/M/MV5BZjJiNjU4NmYtMTA5OS00N2QyLTgxNDMtMmM4OGE5N2RjYzllXkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_SX300.jpg";
+    final String url3="https://m.media-amazon.com/images/M/MV5BOGE4NzU1YTAtNzA3Mi00ZTA2LTg2YmYtMDJmMThiMjlkYjg2XkEyXkFqcGdeQXVyNTgzMDMzMTg@._V1_SX300.jpg";
+    final String url4="https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg";
+    final String url5="https://m.media-amazon.com/images/M/MV5BMTQ3MDM1MDU2NF5BMl5BanBnXkFtZTgwMzM3OTIzNzE@._V1_SX300.jpg";
+    final String url6="https://m.media-amazon.com/images/M/MV5BMjE1NjQ5ODc2NV5BMl5BanBnXkFtZTgwOTM5ODIxNjE@._V1_SX300.jpg";
+    final String url7="https://m.media-amazon.com/images/M/MV5BODA1ZTJjODQtNzZmYi00YThmLTg5N2MtODUwNGFiYjRkNzBlXkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_SX300.jpg";
 
-    //Customised adapter for showing movvvies in recycle view
+    //Customised adapter for showing movies in recycle view
     CustomAdapterMain adapter;
 
     //Recycle view for movies
@@ -53,6 +55,10 @@ public class MainActivity extends AppCompatActivity {
         titleList.add("Top 10 in India Today");
         titleList.add("Popular on Movies Hub");
         titleList.add("US TV Shows");
+        titleList.add("Movies Hub Originals");
+        titleList.add("Bollywood Hits");
+        titleList.add("TV Comedies");
+        titleList.add("New Releases");
 
 
         //Adding Movies Poster
@@ -61,6 +67,8 @@ public class MainActivity extends AppCompatActivity {
         movieList.add(url3);
         movieList.add(url4);
         movieList.add(url5);
+        movieList.add(url6);
+        movieList.add(url7);
 
 
 
